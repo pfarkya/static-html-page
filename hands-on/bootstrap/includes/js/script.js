97b77742282c34da21cda3f3793d90ccb0160328
Prashant Farkya <prashant.farkya@gmail.com>
2016-07-05 18:43:46 +0530

    some changes

--- hands-on/bootstrap/includes/js/script.js
+++ hands-on/bootstrap/includes/js/script.js
@@ -16,6 +16,10 @@ $(function(){
   	e.preventDefault();
   	$('#successAlert').slideDown();
   });
-
+  $("a.pop").click(function(e){
+  	e.preventDefault();
+  });
+  $("a.pop").popover();
+  $('[rel="tooltip"]').tooltip();
 
 });
